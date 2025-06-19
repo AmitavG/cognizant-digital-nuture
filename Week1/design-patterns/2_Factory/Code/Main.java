d8f613315ac4c98eaee9e303bc82fc0aba2cee48
AmitavG <amitavgodishala@gmail.com>
2025-06-20 01:18:03 +0530

    Add Main class

---
+++ Week1/design-patterns/2_Factory/Code/Main.java
@@ -0,0 +1,17 @@
+package com.factory;
+
+public class Main {
+	public static void main(String[] args) {
+		DocumentFactory word = new WordDocumentFactory();
+		Document wordDoc = word.createDocument();
+		wordDoc.open();
+		
+		DocumentFactory pdf = new PdfDocumentFactory();
+		Document pdfDoc = pdf.createDocument();
+		pdfDoc.open();
+		
+		DocumentFactory excel = new ExcelDocumentFactory();
+		Document excelDoc = excel.createDocument();
+		excelDoc.open();
+	}
+}
