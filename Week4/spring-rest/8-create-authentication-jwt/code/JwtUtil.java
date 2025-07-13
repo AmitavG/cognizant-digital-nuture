d221df4d19d87bb1311671c2bcfe6577e0027795
AmitavG <amitavgodishala@gmail.com>
2025-07-13 17:48:56 +0530

    Add jwtutil class

---
+++ Week4/spring-rest/8-create-authentication-jwt/code/JwtUtil.java
@@ -0,0 +1,24 @@
+package com.cognizant.springlearn.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256); // Symmetric key
+
+    public String generateToken(String username) {
+        long now = System.currentTimeMillis();
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + 1000 * 60 * 10)) // 10 minutes
+                .signWith(key)
+                .compact();
+    }
+}
