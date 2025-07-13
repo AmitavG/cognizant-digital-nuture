11c8332f250361c5752fbe2d4780897b9bfe122a
AmitavG <amitavgodishala@gmail.com>
2025-07-13 17:28:28 +0530

    Add Controller Main

---
+++ Week4/spring-rest/6-rest-country-web/code/ControllerDemo.java
@@ -0,0 +1,11 @@
+package com.cognizant.springlearn.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerDemo {
+	public static void main( String[] args ){
+        SpringApplication.run(ControllerDemo.class, args);
+    }
+}
